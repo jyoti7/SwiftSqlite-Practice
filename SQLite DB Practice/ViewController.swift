11901d31647e271ee56bf7ef3feb0ebe1a9c1234
jyoti7 <jotey77@gmail.com>
2019-02-04 16:37:59 +0600

    All Button added

--- SQLite DB Practice/ViewController.swift
+++ SQLite DB Practice/ViewController.swift
@@ -12,9 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func createTable(_ sender: Any) {
+        print("Create Table Tapped")
+    }
+    
+    @IBAction func insertUser(_ sender: Any) {
+         print("Insert user Tapped")
+    }
+    
+    @IBAction func listUsers(_ sender: Any) {
+        print("List users Tapped")
+    }
+    
+    @IBAction func updateUser(_ sender: Any) {
+        print("Update user Tapped")
+    }
+    
+    @IBAction func deleteUser(_ sender: Any) {
+        print("Delete user Tapped")
+    }
+    
+    
+    
 }
 
